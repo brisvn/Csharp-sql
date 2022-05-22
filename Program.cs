d49c1551b47acdf356000a3fe65e5fb0c3e74b0c
Brisvn <Brisvn@outlook.com>
2022-05-22 00:37:01 -0400

    Added command line insertion of data, next need to write the function to push to the SQL DB

--- Program.cs
+++ Program.cs
@@ -8,21 +8,28 @@ namespace ConnectingToSQLServer
     {
         static void Main(string[] args)
         {
+            // connect to SQL server
+            establishSQLConn();
+            // Get info from user
+            Unit currentUnit = getUserInput();
+            Console.WriteLine(currentUnit);
+            addUnit(currentUnit);
+                
+        }
+        static void establishSQLConn(){
+
             Console.WriteLine("Getting Connection ...");
 
             var datasource = @"localhost";//your server
-            var database = "DatabaseName"; //your database name
+            var database = "Units"; //your database name
             var username = "sa"; //username of server to connect
-            var password = "S@LPword"; //password
+            var password = "1Secure*Password1"; //password
 
             //your connection string 
             string connString = @"Data Source=" + datasource + ";Initial Catalog="
                         + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
-
             //create instanace of database connection
             SqlConnection conn = new SqlConnection(connString);
-           
-
             try
             {
                 Console.WriteLine("Openning Connection ...");
@@ -36,33 +43,63 @@ namespace ConnectingToSQLServer
             {
                 Console.WriteLine("Error: " + e.Message);
             }
-            //create a new SQL Query using StringBuilder
-                StringBuilder strBuilder = new StringBuilder();
-                strBuilder.Append("INSERT INTO Abilities (Unit_Name, Ability) VALUES ");
-                
-                Unit archon = new Unit("Archon", 8, 2, 2, 3, 3, 5, 5, 9, 5);
-                List<string> abilityArray = new List<string>();
-                abilityArray.Add("First Ability: This");
-                abilityArray.Add("Second Ability: That");
-                Abilities archonAbility = new Abilities(abilityArray, "Archon");
-                // Console.WriteLine(archon.name);
-                // strBuilder.Append(archon);
-                string Last = archonAbility.abilities.Last();
-                foreach(string abi in archonAbility.abilities){
-                    string str = $"('{archonAbility.name}', '{abi}'";
-                    strBuilder.Append(str+")");
-                    if(abi != Last){
-                        strBuilder.Append(",");
+        }
+        static Unit getUserInput(){
+                Unit newUnit = new Unit();
+                Console.WriteLine("Would you like to input a Unit? (Y/N)");
+                string response = Console.ReadLine();
+                if(response == "Y"){
+                    Console.WriteLine("What is the name of your Unit?");
+                    response = Console.ReadLine();
+                    newUnit.name = response;
+                    bool input_corr = false;
+                    while(input_corr == false){
+                        Console.WriteLine("Insert the stats of your unit 1 at a time in order of M, WS, BS, S, T, W, A, Ld, Sv");
+                        response = Console.ReadLine();
+                        string[] str_arr=response.Split(" ").ToArray();
+                        int[] int_arr= Array.ConvertAll(str_arr,Int32.Parse);
+                        if(int_arr.Length < 9){
+                            Console.WriteLine("Not enough inputs");
+                        }
+                        else{
+                            newUnit.M = int_arr[0];
+                            newUnit.WS = int_arr[1];
+                            newUnit.BS = int_arr[2];
+                            newUnit.S = int_arr[3];
+                            newUnit.T = int_arr[4];
+                            newUnit.W = int_arr[5];
+                            newUnit.A = int_arr[6];
+                            newUnit.Ld = int_arr[7];
+                            newUnit.Sv = int_arr[8];
+                            input_corr = true;
+                        }
                     }
+                    List<string> abilityArray = new List<string>();
+                    Console.WriteLine("Does your unit have any abilities? (Y/N)");
+                    response = Console.ReadLine();
+                    if(response == "Y"){
+                        Console.WriteLine("Enter the first ability");
+                        response = Console.ReadLine();
+                        abilityArray.Add(response);
+                        bool more = true;
+                        while(more){
+                            Console.WriteLine("Are there more abilites? (Y/N)");
+                            response = Console.ReadLine();
+                            if(response == "Y"){
+                                Console.WriteLine("Enter the next ability");
+                                response = Console.ReadLine();
+                                abilityArray.Add(response);
+                            }
+                            else{
+                                more = false;
+                            }
+                        }
+                    }
+                    Abilities newAbility = new Abilities(abilityArray, newUnit.name);
+                    newUnit.abilities = newAbility;
+                    return newUnit;
                 }
-
-                Console.WriteLine(strBuilder.ToString());
-                string sqlQuery = strBuilder.ToString();
-                using (SqlCommand command = new SqlCommand(sqlQuery, conn)) //pass SQL query created above and connection
-                {
-                    command.ExecuteNonQuery(); //execute the Query
-                    Console.WriteLine("Query Executed.");
-                }
+                return newUnit;
         }
         class Unit{
             public string name;
@@ -75,6 +112,7 @@ namespace ConnectingToSQLServer
             public int A;
             public int Ld;
             public int Sv;
+            public Abilities abilities;
             public Unit(){
                 name = "";
                 M = 0;
@@ -86,8 +124,9 @@ namespace ConnectingToSQLServer
                 A = 0;
                 Ld = 0;
                 Sv = 0;
+                abilities = new Abilities();
             }
-            public Unit(string unitName, int unitM, int unitWS, int unitBS, int unitS, int unitT, int unitW, int unitA, int unitLd, int unitSv){
+            public Unit(string unitName, int unitM, int unitWS, int unitBS, int unitS, int unitT, int unitW, int unitA, int unitLd, int unitSv, Abilities unitAbilities){
                 name = unitName;
                 M = unitM;
                 WS = unitWS;
@@ -98,6 +137,7 @@ namespace ConnectingToSQLServer
                 A = unitA;
                 Ld = unitLd;
                 Sv = unitSv;
+                abilities = unitAbilities;
             }
             public override string ToString(){
                 return $"('{name}', '{M}', '{WS}', '{BS}', '{S}','{T}','{W}','{A}','{Ld}','{Sv}')";
@@ -118,8 +158,25 @@ namespace ConnectingToSQLServer
                 }
             }
         }
-        static void AddUnit(Unit unit_details){
+        static void addUnit(Unit unit_details){
+            
+            // StringBuilder strBuilder = new StringBuilder();
+            // string Last = archonAbility.abilities.Last();
+            //     foreach(string abi in archonAbility.abilities){
+            //         string str = $"('{archonAbility.name}', '{abi}'";
+            //         strBuilder.Append(str+")");
+            //         if(abi != Last){
+            //             strBuilder.Append(",");
+            //         }
+            //     }
 
+            //     Console.WriteLine(strBuilder.ToString());
+            //     string sqlQuery = strBuilder.ToString();
+            //     using (SqlCommand command = new SqlCommand(sqlQuery, conn)) //pass SQL query created above and connection
+            //     {
+            //         command.ExecuteNonQuery(); //execute the Query
+            //         Console.WriteLine("Query Executed.");
+            //     }
         }
     }
 }
